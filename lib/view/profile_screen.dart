6ab5bab43d587c55c5cbbb9df2fd2ec7a73106c7
omer358 <omermaki358@gmail.com>
2024-04-04 13:51:29 +0200

    close issue #1

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -10,169 +10,168 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<ProfileViewModel>(
         init: Get.find<ProfileViewModel>(),
-        builder: (controller) {
-          return controller.loading.value
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : SafeArea(
-                  child: Container(
-                    padding: const EdgeInsets.fromLTRB(16, 32, 16, 8),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Container(
-                                  width: 120,
-                                  height: 120,
-                                  decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.all(
-                                      Radius.circular(100),
-                                    ),
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                          controller.userModel!.pic),
-                                    ),
-                                    color: Colors.green.shade500,
+        builder: (controller) => controller.loading.value
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(8, 32, 16, 8),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(100),
                                   ),
+                                  image: DecorationImage(
+                                    image:
+                                        NetworkImage(controller.userModel!.pic),
+                                  ),
+                                  color: Colors.green.shade500,
                                 ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    CustomText(
-                                        text: controller.userModel!.name!,
-                                        fontSize: 32),
-                                    const SizedBox(
-                                      height: 8,
-                                    ),
-                                    CustomText(
-                                        text: controller.userModel!.email,
-                                        fontSize: 18)
-                                  ],
-                                )
-                              ],
+                              ),
+                              const SizedBox(width: 20,),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CustomText(
+                                      text: controller.userModel!.name!,
+                                      fontSize: 32),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  CustomText(
+                                      text: controller.userModel!.email,
+                                      fontSize: 18)
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.edit,
+                              color: Colors.green.shade500,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 100,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.edit,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(
-                                  text: "Edit Profile", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {},
+                            title:
+                                CustomText(text: "Edit Profile", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
                             ),
+                            onTap: () {},
                           ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.edit_location,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(
-                                  text: "Shipping Address", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {},
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.edit_location,
+                              color: Colors.green.shade500,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.history,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(
-                                  text: "Order History", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {},
+                            title: CustomText(
+                                text: "Shipping Address", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
                             ),
+                            onTap: () {},
                           ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.credit_card,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(text: "Cards", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {},
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.history,
+                              color: Colors.green.shade500,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.notifications,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(
-                                  text: "Notifications", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {},
+                            title:
+                                CustomText(text: "Order History", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
                             ),
+                            onTap: () {},
                           ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            child: ListTile(
-                              leading: Icon(
-                                Icons.logout,
-                                color: Colors.green.shade500,
-                              ),
-                              title: CustomText(text: "Logout", fontSize: 18),
-                              trailing: Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.black,
-                              ),
-                              onTap: () {
-                                controller.signOut();
-                              },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.credit_card,
+                              color: Colors.green.shade500,
                             ),
+                            title: CustomText(text: "Cards", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
+                            ),
+                            onTap: () {},
                           ),
-                          const SizedBox(
-                            height: 15,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.notifications,
+                              color: Colors.green.shade500,
+                            ),
+                            title:
+                                CustomText(text: "Notifications", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
+                            ),
+                            onTap: () {},
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.logout,
+                              color: Colors.green.shade500,
+                            ),
+                            title: CustomText(text: "Logout", fontSize: 18),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black,
+                            ),
+                            onTap: () {
+                              controller.signOut();
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                      ],
                     ),
                   ),
-                );
-        });
+                ),
+              ));
   }
 }
