a5a75a4c636c02506584e95210e380ae8b17c896
omer358 <omermaki358@gmail.com>
2024-04-09 03:52:50 +0200

    close #3

--- lib/view_model/checkout_view_model.dart
+++ lib/view_model/checkout_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:e_commerce_app/view/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,6 +9,7 @@ class CheckoutViewModel extends GetxController {
   int _currentStep = 0;
   late String street1, street2, city, state, country;
   GlobalKey<FormState> formState = GlobalKey();
+
   get currentStep => _currentStep;
 
   void onStepTapped(int newIndex) {
@@ -16,12 +18,20 @@ class CheckoutViewModel extends GetxController {
   }
 
   void nextStep() {
-    log(_currentStep.toString());
-    if (_currentStep < 2) {
+    const int maxStep = 2;
+
+    if (_currentStep == 1) {
+      // Check if it's the step with the form
+      if (formState.currentState!.validate()) {
+        _currentStep += 1;
+      }
+    } else if (_currentStep < maxStep) {
       _currentStep += 1;
     } else {
+      onMaxStepReached();
       log("finish!");
     }
+
     update();
   }
 
@@ -30,5 +40,9 @@ class CheckoutViewModel extends GetxController {
       _currentStep -= 1;
     }
     update();
+  }
+
+  void onMaxStepReached() {
+    Get.off(() => const HomeScreen());
   }
 }
